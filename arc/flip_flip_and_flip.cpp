087488e26a52acdb04ce54a524fdd02c24501087
marubontan <marubontan@gmail.com>
2019-08-25 15:14:20 -0300

    add problems

---
+++ arc/flip_flip_and_flip.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long n, m;
+    cin >> n >> m;
+    if (n == 2 || m == 2){
+        cout << 0 << endl;
+    } else if (n == 1 && m == 1) {
+        cout << 1 << endl;
+    } else if (n == 1){
+        cout << m - 2 << endl;
+    } else if (m == 1){
+        cout << n - 2 << endl;
+    } else {
+        cout << (n-2) * (m-2) << endl;
+    }
+}
