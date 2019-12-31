8caf71eb8d1f1f1dca916479d8f938558e9cbd47
marubontan <marubontan@gmail.com>
2019-12-31 15:05:33 -0300

    add problem

---
+++ mitsui/november_30.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+    if (d == 1){
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
