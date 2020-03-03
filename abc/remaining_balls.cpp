e7a329ea176df139542c771f71667bb359b4cd96
Shuhei Kishi <marubontan@gmail.com>
2020-03-02 21:02:04 -0300

    add problem

---
+++ abc/remaining_balls.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s,t,u;
+    int a, b;
+    cin >> s >> t;
+    cin >> a >> b;
+    cin >> u;
+    if (s == u){
+        a--;
+    } else {
+        b--;
+    }
+    cout << a << " " << b << endl;
+    return 0;
+}
