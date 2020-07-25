f31c5d4a68a547d0fd71dbb9fb2dfe11eed0015d
Shuhei Kishi <marubontan@gmail.com>
2020-07-25 11:02:55 -0300

    add problems

---
+++ abc/registration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s, t;
+    cin>>s;
+    cin>>t;
+    if (s.size() + 1 == t.size() && t.substr(0, s.size()) == s){
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+
+
+}
