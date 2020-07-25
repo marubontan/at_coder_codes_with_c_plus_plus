1f99cb11956983f7db1058810c8f8144d7520f71
Shuhei Kishi <marubontan@gmail.com>
2020-07-25 13:03:17 -0300

    add problem

---
+++ abc/a_question_c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s;cin >> s;
+    if (s == "ARC"){
+        cout << "ABC" << endl;
+    } else {
+        cout << "ARC" << endl;
+    }
+    return 0;
+}
